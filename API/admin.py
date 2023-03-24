665819fa48fb745622dab5758933696aaf2cf98d
somethingwentwrongx <tymon.dydowicz@onet.pl>
2023-03-24 12:56:46 +0100

    Updating admin panel

--- API/admin.py
+++ API/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Patient, Clinic
+from .models import Patient, Clinic, Appointment, Account
 admin.site.register(Patient)
 admin.site.register(Clinic)
+admin.site.register(Appointment)
+admin.site.register(Account)
